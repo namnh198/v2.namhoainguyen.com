b8589fca9fac8c5e24563eea403a1a4165ca1ed7
namnh198 <namnhn98@gmail.com>
2023-10-21 00:42:01 +0700

    design navbar & hero cover

---
+++ src/navigation.ts
@@ -0,0 +1,50 @@
+export interface NavbarItemType {
+  name: string;
+  permalink: string;
+}
+
+export const navigation = [
+  {
+    name: 'Home',
+    permalink: '/'
+  },
+  {
+    name: 'About',
+    permalink: '/about'
+  },
+  {
+    name: 'Blog',
+    permalink: '/blog'
+  },
+  {
+    name: 'Series',
+    permalink: '/series'
+  },
+  {
+    name: 'Tags',
+    permalink: 'tags'
+  }
+] as NavbarItemType[];
+
+export const footerSocial = [
+  {
+    name: 'Facebook',
+    permalink: '',
+    icon: 'fa6-brands:square-facebook'
+  },
+  {
+    name: 'Linkedin',
+    permalink: '',
+    icon: 'fa6-brands:linkedin'
+  },
+  {
+    name: 'Github',
+    permalink: '',
+    icon: 'fa6-brands:square-github'
+  },
+  {
+    name: 'Youtube',
+    permalink: '',
+    icon: 'fa6-brands:square-youtube'
+  }
+]
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,40 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 /** @type {import('tailwindcss').Config} */
-export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
+module.exports = {
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      container: {
+        center: true,
+        padding: {
+          DEFAULT: "1rem",
+          "2xl": "128px",
+        },
+      },
+      fontFamily: {
+        sans: ["Poppins", ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        neutral: {
+          50: '#f9fafb',
+          100: '#f3f4f6',
+          200: '#e5e7eb',
+          300: '#d1d5db',
+          400: '#9ca3af',
+          500: '#6b7280',
+          600: '#4b5563',
+          700: '#374151',
+          800: '#1f2937',
+          900: '#111827',
+        }
+      }
+    },
+  },
+  plugins: [
+    require('@tailwindcss/aspect-ratio'),
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/typography'),
+  ],
 }
